e396ad0dbc419e80c2831ec765b5e5f0ae129626
Bonggeun Jeon <tud.bonggeun@gmail.com>
2024-10-11 13:53:47 +0200

    Practice 1

---
+++ 01.py
@@ -0,0 +1,247 @@
+import torch
+import torchvision
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
+from torch.autograd import Variable
+from torch import optim
+
+import torch.nn as nn
+import torch.nn.functional as F
+import os
+import time
+import cv2
+import random
+from PIL import Image
+from tqdm import tqdm_notebook as tqdm
+
+device = torch.device("cude:0" if torch.cuda.is_available() else "cpu")
+
+#### Dataset ####
+
+class ImageTransform():
+    def __init__(self, resize, mean, std):
+        self.data_transform = {
+            'train': transforms.Compose([
+                transforms.RandomResizedCrop(resize, scale=(0.5, 1.0)), # 면적 비율만큼 무작위로 자름
+                transforms.RandomHorizontalFlip(), # Horizontal = 좌우반전 
+                transforms.ToTensor(), # Pytorch 기본 데이터 형식 - Tensor
+                transforms.Normalize(mean, std) # 0 ~ 255 (ToTensor) -> 0 ~ 1 (Normalized) -> -1 ~ 1
+            ]),
+            'val': transforms.Compose([
+                transforms.Resize(256),
+                transforms.CenterCrop(resize),
+                transforms.ToTensor(),
+                transforms.Normalize(mean, std)
+            ])
+        }
+    
+    def __call__(self, img, phase):
+        return self.data_transform[phase](img)
+
+
+cat_directory = '../'
+dog_directory = '../'
+
+cat_image_filepaths = sorted([os.path.join(cat_directory, f) for f in os.listdir(cat_directory)])
+dog_image_filepaths = sorted([os.path.join(dog_directory, f) for f in os.listdir(dog_directory)])
+
+image_filepaths = [*cat_image_filepaths, *dog_image_filepaths]
+correct_images_filepaths = [i for i in image_filepaths if cv2.imread(i) is not None]
+
+random.seed(42)
+random.shuffle(correct_images_filepaths)
+train_images_filepaths = correct_images_filepaths[:400]
+val_images_filepaths = correct_images_filepaths[400:-10]
+test_images_filepaths = correct_images_filepaths[-10:]
+#print(len(train_images_filepaths), len(val_images_filepaths), len(test_images_filepaths))
+
+
+class DogvsCatDataset(Dataset):
+    def __init__(self, file_list, transform=None, phase='train'):
+        self.file_list = file_list # 이미지 데이터가 위치한 파일 경로
+        self.transform = transform # 이미지 데이터 전처리
+        self.phase = phase # self.phase -> ImageTransform() 에서 정의한 'train'과 'val'
+    
+    def __len__(self):
+        return len(self.file_list)
+    
+    def __getitem__(self, idx):
+        img_path = self.file_list[idx] # Image 데이터 Index 가져오기
+        img = Image.open(img_path)
+        img_transformed = self.transform(img, self.phase)
+
+        label = img_path.split('/')[-1].split('.')[0] # Lable 값 가져오기
+        if label == 'dog':
+            label = 1
+        elif label == 'cat':
+            label = 0
+            
+        return img_transformed, label
+
+size = 256
+mean = (0.485, 0.456, 0.406)
+std = (0.229, 0.224, 0.225)
+batch_size = 32
+
+train_dataset = DogvsCatDataset(train_images_filepaths, transform=ImageTransform(size, mean, std), phase='train')
+val_dataset = DogvsCatDataset(val_images_filepaths, transform=ImageTransform(size, mean, std), phase='val')
+test_dataset = DogvsCatDataset(val_images_filepaths, transform=ImageTransform(size, mean, std), phase='val')
+
+index = 0
+#print(train_dataset.__getitem__(index)[0].size())
+#print(train_dataset.__getitem__(index)[1])
+
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+dataloader_dict = {'train': train_dataloader, 'val': val_dataloader}
+
+batch_iterator = iter(train_dataloader)
+inputs, label = next(batch_iterator)
+#print(inputs.size())
+#print(label)
+
+#### Model ####
+class AlexNet(nn.Module):
+    def __init__(self) -> None:
+        super(AlexNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.avgpool == nn.AdaptiveAvgPool2d((6, 6))
+        self.classifier =- nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256*6*6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512, 2),
+        )
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+model = AlexNet()
+model.to(device)
+
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+criterion = nn.CrossEntropyLoss()
+
+def train_model(model, dataloader_dict, criterion, optimizer, num_epoch):
+    
+    since = time.time()
+    best_acc = 0.0
+    
+    for epoch in range(num_epoch):
+        print('Epoch {}/{}'.format(epoch+1, num_epoch))
+        print('-'*20)
+        
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()
+            else:
+                model.eval()
+                
+            epoch_loss = 0.0
+            epoch_corrects = 0
+        
+        for inputs, labels in tqdm(dataloader_dict[phase]):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            optimizer.zero_grad()
+            
+            with torch.set_grad_enabled(phase == 'train'):
+                outputs = model(inputs)
+                _, preds = torch.max(outputs, 1)
+                loss = criterion(outputs, labels)
+                
+                if phase == 'train':
+                    loss.backward()
+                    optimizer.step()
+
+                epoch_loss += loss.item() * inputs.size(0)
+                epoch_corrects += torch.sum(preds == labels.data)
+        
+        epoch_loss = epoch_loss / len(dataloader_dict[phase].dataset)
+        epoch_acc = epoch_corrects.double() / len(dataloader_dict[phase].dataset)
+        
+        print('{} Loss : {:.4f} Acc : {:.4f}'.format(phase, epoch_loss, epoch_acc))
+        
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    
+    return model
+
+num_epoch = 10
+model = train_model(model, dataloader_dict, criterion, optimizer, num_epoch)
+
+import pandas as pd
+
+id_list = []
+pred_list = []
+_id = 0
+
+with torch.no_grad():
+    for test_path in tqdm(test_images_filepaths):
+        img = Image.open(test_path)
+        _id = test_path.split('/')[-1].split('.')[1]
+        transform = ImageTransform(size, mean, std)
+        img = transform(img, phase='val')
+        img = img.unsqueeze(0)
+        img = img.to(device)
+        
+        model.eval()
+        outputs = model(img)
+        preds = F.softmax(outputs, dim=1)[:, 1].tolist()
+        
+        id_list.append(_id)
+        pred_list.append(preds[0])
+        
+res = pd.DataFrame({
+    'id' : id_list,
+    'label' : pred_list
+})
+res.to_csv('../a.csv', index=False)
+res.head(10)
+
+class_ = classes = {0:'cat', 1:'dog'}
+
+def display_image_grid(images_filepaths, predicted_labels=(), cols=5):
+    rows = len(image_filepaths)
+    figure, ax = plt.subplots(nrow=rows, ncols=cols, figsize=(12, 6))
+    for i, image_filepaths in enumerate(images_filepaths):
+        image = cv2.imread(image_filepaths)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        
+        a = random.choice(res['id'].values)
+        label = res.loc[res['id'] == a, 'label'].values[0]
+        if label > 0.5 :
+            label = 1
+        else:
+            label = 0
+            
+        ax.ravel()[i].imshow(image)
+        ax.ravel()[i].set_title(class_[label])
+        ax.ravel()[i].set_axis_off()
+    plt.tight_layout()
+    plt.show()
+
+display_image_grid(test_images_filepaths)
